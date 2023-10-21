1b8d4369aa501d8d2e6a34253dd0444253e2f5d7
VASYL\chrep <vasylkhreptak12@gmail.com>
2023-10-21 11:30:11 +0300

    Updated MainEntryPoint;

--- Assets/Scripts/Infrastructure/EntryPoints/MainEntryPoint.cs
+++ Assets/Scripts/Infrastructure/EntryPoints/MainEntryPoint.cs
@@ -1,10 +1,25 @@
-﻿using Infrastructure.EntryPoints.Core;
+﻿using Cinemachine;
+using Infrastructure.EntryPoints.Core;
 using UnityEngine;
+using Zenject;
 
 namespace Infrastructure.EntryPoints
 {
     public class MainEntryPoint : MonoBehaviour, IEntryPoint
     {
+        [Header("References")]
+        [SerializeField] private Transform _playerSpawnPoint;
+        [SerializeField] private GameObject _playerPrefab;
+        [SerializeField] private GameObject _cameraPrefab;
+
+        private IInstantiator _instantiator;
+
+        [Inject]
+        private void Constructor(IInstantiator instantiator)
+        {
+            _instantiator = instantiator;
+        }
+
         #region MonoBehaviour
 
         private void Start()
@@ -14,6 +29,26 @@ namespace Infrastructure.EntryPoints
 
         #endregion
 
-        public void Enter() { }
+        public void Enter()
+        {
+            Transform player = InitializePlayer();
+            InitializeCamera(player);
+        }
+
+        private Transform InitializePlayer()
+        {
+            Transform player = _instantiator.InstantiatePrefab(_playerPrefab).transform;
+            player.position = _playerSpawnPoint.position;
+            player.rotation = _playerSpawnPoint.rotation;
+            return player;
+        }
+
+        private void InitializeCamera(Transform target)
+        {
+            GameObject cameraRoot = _instantiator.InstantiatePrefab(_cameraPrefab);
+            CinemachineVirtualCamera virtualCamera = cameraRoot.GetComponentInChildren<CinemachineVirtualCamera>(true);
+            virtualCamera.Follow = target;
+            virtualCamera.LookAt = target;
+        }
     }
 }
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,4 +1,5 @@
-﻿using Zenject;
+﻿using Cinemachine;
+using Zenject;
 
 public class Test : IInitializable
 {
