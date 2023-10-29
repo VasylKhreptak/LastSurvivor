b1d0c7f1f51e43eeaf61ef6ed134811b97b25446
VASYL\chrep <vasylkhreptak12@gmail.com>
2023-10-29 22:07:07 +0200

    Updated Player.cs;

--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -1,6 +1,18 @@
 ﻿using UnityEngine;
+using Zenject;
 
 namespace Entities.Player
 {
-    public class Player : MonoBehaviour { }
+    public class Player : MonoBehaviour
+    {
+        private Transform _transform;
+
+        [Inject]
+        private void Constructor(Transform transform)
+        {
+            _transform = transform;
+        }
+
+        public Transform Transform => _transform;
+    }
 }
