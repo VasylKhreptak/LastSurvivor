b04707b0450490aac9f3d3f3e5d61e71c9233258
VASYL\chrep <vasylkhreptak12@gmail.com>
2023-11-06 09:09:13 +0200

    Added Resources Text Views;

---
+++ Assets/Scripts/UI/GameResources/MoneyText.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Infrastructure.Services.PersistentData.Core;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+using Resources = Data.Persistent.Resources;
+
+namespace UI.GameResources
+{
+    public class MoneyText : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private TMP_Text _tmp;
+
+        private Resources _resources;
+
+        [Inject]
+        private void Constructor(IPersistentDataService persistentDataService)
+        {
+            _resources = persistentDataService.PersistentData.PlayerData.Resources;
+        }
+
+        private IDisposable _subscription;
+
+        #region MonoBehaviour
+
+        private void OnValidate() => _tmp ??= GetComponent<TMP_Text>();
+
+        private void OnEnable() => StartObserving();
+
+        private void OnDisable() => StopObserving();
+
+        #endregion
+
+        private void StartObserving() => _subscription = _resources.Money.Value.Subscribe(OnValueChanged);
+
+        private void StopObserving() => _subscription?.Dispose();
+
+        private void OnValueChanged(int value) => _tmp.text = value.ToString();
+    }
+}
