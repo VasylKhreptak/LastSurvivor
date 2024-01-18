117510774aab5f153c257391a638de37421ed47f
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-01-18 12:55:18 +0200

    - added SceneAudioCleaner;

---
+++ Assets/Scripts/Audio/SceneAudioCleaner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Plugins.AudioService.Core;
+using UnityEngine;
+using UnityEngine.Audio;
+using Zenject;
+
+namespace Audio
+{
+    public class SceneAudioCleaner : MonoBehaviour
+    {
+        [Header("Preferences")]
+        [SerializeField] private List<AudioMixerGroup> _targetMixerGroups;
+
+        private HashSet<AudioMixerGroup> _targetMixerGroupsSet;
+
+        private IAudioService _audioService;
+
+        [Inject]
+        private void Constructor(IAudioService audioService)
+        {
+            _audioService = audioService;
+        }
+
+        #region MonoBehaviour
+
+        private void Awake() => _targetMixerGroupsSet = new HashSet<AudioMixerGroup>(_targetMixerGroups);
+
+        private void OnDestroy() => Clear();
+
+        #endregion
+
+        private void Clear() => _audioService.Stop(x => _targetMixerGroupsSet.Contains(x.AudioMixerGroup));
+    }
+}
