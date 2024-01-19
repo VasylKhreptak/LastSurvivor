12a1995e497c1bac04db8d1d788264d3ba672979
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-01-19 18:12:15 +0200

    - updated NavMeshSurface;

--- Assets/Scripts/Gameplay/Entities/Player/StateMachine/States/IdleState.cs
+++ Assets/Scripts/Gameplay/Entities/Player/StateMachine/States/IdleState.cs
@@ -1,14 +1,10 @@
 ﻿using Gameplay.Entities.Player.StateMachine.States.Core;
 using Infrastructure.StateMachine.Main.States.Core;
-using UnityEngine;
 
 namespace Gameplay.Entities.Player.StateMachine.States
 {
     public class IdleState : IPlayerState, IState
     {
-        public void Enter()
-        {
-            Debug.Log("Enter Idle State");
-        }
+        public void Enter() { }
     }
 }
