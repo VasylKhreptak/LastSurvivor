05762bba09676d1c4eeada1fc219fd7225a96764
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-01-19 09:38:49 +0200

    - added helicopter sound;

---
+++ Assets/Scripts/Audio/Players/MonoAudioPlayer.cs
@@ -0,0 +1,31 @@
+﻿using Plugins.AudioService.Core;
+using UnityEngine;
+using Zenject;
+
+namespace Audio.Players
+{
+    public class MonoAudioPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioPlayer.Preferences _preferences;
+
+        private IAudioService _audioService;
+
+        [Inject]
+        private void Constructor(IAudioService audioService)
+        {
+            _audioService = audioService;
+        }
+
+        private AudioPlayer _audioPlayer;
+
+        #region MonoBehaviour
+
+        private void Awake() => _audioPlayer = new AudioPlayer(_audioService, _preferences);
+
+        private void OnEnable() => _audioPlayer.Play(transform.position);
+
+        private void OnDisable() => _audioPlayer.Stop();
+
+        #endregion
+    }
+}
