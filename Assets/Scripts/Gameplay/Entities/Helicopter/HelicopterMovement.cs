30466f358e5aa7d1cfef0348d4b7c7f19e4d096a
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-01-20 23:30:12 +0200

    - added helicopter movement;

--- Assets/Scripts/Gameplay/Entities/Helicopter/HelicopterMovement.cs
+++ Assets/Scripts/Gameplay/Entities/Helicopter/HelicopterMovement.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Gameplay.Entities.Player;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Splines;
 using Zenject;
 
 namespace Gameplay.Entities.Helicopter
@@ -11,22 +13,67 @@ namespace Gameplay.Entities.Helicopter
         private readonly PlayerHolder _playerHolder;
         private readonly Preferences _preferences;
 
-        public HelicopterMovement(Transform transform, Preferences preferences)
+        private readonly Spline _spline;
+
+        public HelicopterMovement(Transform transform, PlayerHolder playerHolder, Preferences preferences)
         {
             _transform = transform;
+            _playerHolder = playerHolder;
             _preferences = preferences;
+            _spline = _preferences.SplineContainer.Spline;
         }
 
-        public void Tick() { }
+        private Vector3 _lastPlayerPosition;
+        private Vector3 _targetPosition;
+
+        private Quaternion _targetRotation;
+
+        public void Tick()
+        {
+            UpdateLastPlayerPosition();
+            UpdateTargetValues();
+            MoveHelicopter();
+        }
+
+        private void UpdateLastPlayerPosition()
+        {
+            if (_playerHolder.Instance == null)
+                return;
+
+            _lastPlayerPosition = _playerHolder.Instance.transform.position;
+        }
+
+        private void UpdateTargetValues()
+        {
+            Vector3 localPoint = _preferences.SplineContainer.transform.InverseTransformPoint(_lastPlayerPosition);
+            SplineUtility.GetNearestPoint(_spline, localPoint, out float3 _, out float nearestTime);
+            float targetSplineDistance = nearestTime * _spline.GetLength() + _preferences.DistanceOffset;
+            Vector3 targetSplinePoint = _spline.GetPointAtLinearDistance(0, targetSplineDistance, out float _);
+            _targetPosition = _preferences.SplineContainer.transform.TransformPoint(targetSplinePoint);
+            _targetRotation = Quaternion.LookRotation(_spline.EvaluateTangent(nearestTime));
+        }
+
+        private void MoveHelicopter()
+        {
+            _transform.position = Vector3.Lerp(_transform.position, _targetPosition, Time.deltaTime * _preferences.FollowSpeed);
+            _transform.rotation = Quaternion.Lerp(_transform.rotation, _targetRotation * Quaternion.Euler(_preferences.RotationOffset),
+                Time.deltaTime * _preferences.RotateSpeed);
+        }
 
         [Serializable]
         public class Preferences
         {
+            [SerializeField] private SplineContainer _splineContainer;
             [SerializeField] private float _followSpeed = 1f;
-            [SerializeField] private float _distanceOffset = 5f;
+            [SerializeField] private float _rotateSpeed = 1f;
+            [SerializeField] private float _distanceOffset = -5f;
+            [SerializeField] private Vector3 _rotationOffset = Vector3.zero;
 
+            public SplineContainer SplineContainer => _splineContainer;
             public float FollowSpeed => _followSpeed;
+            public float RotateSpeed => _rotateSpeed;
             public float DistanceOffset => _distanceOffset;
+            public Vector3 RotationOffset => _rotationOffset;
         }
     }
 }
