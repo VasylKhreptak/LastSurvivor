5071f7fc3d5be9d26523f5012f0907475d19adf1
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-02-04 14:18:35 +0200

    - updated Player;

--- Assets/Scripts/Gameplay/Entities/Player/Player.cs
+++ Assets/Scripts/Gameplay/Entities/Player/Player.cs
@@ -1,17 +1,13 @@
-﻿using Gameplay.Entities.Health.Core;
-using UnityEngine;
+﻿using UnityEngine;
 using Zenject;
 
 namespace Gameplay.Entities.Player
 {
     public class Player : MonoBehaviour
     {
-        private IHealth _health;
-
         [Inject]
-        private void Constructor(IHealth health, PlayerWaypointNavigator waypointNavigator)
+        private void Constructor(PlayerWaypointNavigator waypointNavigator)
         {
-            _health = health;
             WaypointNavigator = waypointNavigator;
         }
 
