778b74640cd853f187b6ff347a1e462c98bb251e
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-02-12 15:36:01 +0200

    - added EnableInstancingMenu; - enabled GPU instancing in all materials;

---
+++ Assets/Scripts/Editor/Utility/EnableInstancingMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Utility
+{
+    public static class EnableInstancingMenu
+    {
+        [MenuItem("Game/Utility/Enable GPU instancing in all material")]
+        public static void OpenGameConfig()
+        {
+            string[] allMaterialGuids = AssetDatabase.FindAssets("t:Material");
+
+            foreach (string materialGuid in allMaterialGuids)
+            {
+                string materialPath = AssetDatabase.GUIDToAssetPath(materialGuid);
+                Material material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+
+                if (material != null && material.enableInstancing == false)
+                {
+                    material.enableInstancing = true;
+                    Debug.Log("GPU instancing enabled for material: " + material.name);
+                    EditorUtility.SetDirty(material);
+                }
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            Debug.Log("GPU instancing enabled for all supported materials in the project.");
+        }
+    }
+}
