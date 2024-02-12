c2f74444626ebd514fc7c8c9997fbdf822fc869a
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-02-12 14:35:11 +0200

    - fixed soldier weapon collider;

--- Assets/Scripts/Gameplay/Entities/Platoon/Platoon.cs
+++ Assets/Scripts/Gameplay/Entities/Platoon/Platoon.cs
@@ -8,6 +8,7 @@ namespace Gameplay.Entities.Platoon
     public class Platoon : MonoBehaviour
     {
         private List<Transform> _soldierPoints;
+        public SplineTargetFollower TargetFollower { get; private set; }
 
         [Inject]
         private void Constructor(List<Transform> soldierPoints, SplineTargetFollower splineTargetFollower)
@@ -17,7 +18,6 @@ namespace Gameplay.Entities.Platoon
         }
 
         public IReadOnlyList<Transform> SoldierPoints => _soldierPoints;
-        public SplineTargetFollower TargetFollower { get; private set; }
 
         public readonly List<Soldier.Soldier> Soldiers = new List<Soldier.Soldier>();
     }
