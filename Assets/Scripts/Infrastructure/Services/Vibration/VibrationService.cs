3168f9f89e5f6a6bd9d698b5834e023d14c9c6c0
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-02-14 13:10:17 +0200

    - added log service to vibration service;

--- Assets/Scripts/Infrastructure/Services/Vibration/VibrationService.cs
+++ Assets/Scripts/Infrastructure/Services/Vibration/VibrationService.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Services.PersistentData.Core;
+﻿using Infrastructure.Services.Log.Core;
+using Infrastructure.Services.PersistentData.Core;
 using Infrastructure.Services.Vibration.Core;
 using Lofelt.NiceVibrations;
 
@@ -7,15 +8,23 @@ namespace Infrastructure.Services.Vibration
     public class VibrationService : IVibrationService
     {
         private readonly IPersistentDataService _persistentDataService;
+        private readonly ILogService _logService;
 
-        public VibrationService(IPersistentDataService persistentDataService) => _persistentDataService = persistentDataService;
+        public VibrationService(IPersistentDataService persistentDataService, ILogService logService)
+        {
+            _persistentDataService = persistentDataService;
+            _logService = logService;
+        }
 
         public void Vibrate(HapticPatterns.PresetType preset) => TryVibrate(preset);
 
         private void TryVibrate(HapticPatterns.PresetType preset)
         {
             if (_persistentDataService.PersistentData.SettingsData.IsVibrationEnabled)
+            {
                 HapticPatterns.PlayPreset(preset);
+                _logService.Log($"Vibrated with preset: {preset.ToString()}");
+            }
         }
     }
 }
