9aee1491786ebb476545fd20feff9703272f627d
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-02-14 15:32:13 +0200

    - added worker hammer hit sound;

---
+++ Assets/Scripts/Audio/AudioListenerFollower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class AudioListenerFollower : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private AudioListener _audioListener;
+        [SerializeField] private Transform _target;
+
+        private Transform _listenerTransform;
+
+        #region MonoBehaviour
+
+        private void Awake()
+        {
+            _listenerTransform = _audioListener.transform;
+
+            _listenerTransform.SetParent(null);
+            _listenerTransform.rotation = Quaternion.identity;
+            _listenerTransform.position = _target.position;
+        }
+
+        private void Update() => _listenerTransform.position = _target.position;
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Main/Platforms/DumpPlatform/Workers/WorkerHammerSoundPlayer.cs
@@ -0,0 +1,30 @@
+﻿using Audio.Players;
+using Plugins.AudioService.Core;
+using UnityEngine;
+using Zenject;
+
+namespace Main.Platforms.DumpPlatform.Workers
+{
+    public class WorkerHammerSoundPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioPlayer.Preferences _audioPreferences;
+
+        private IAudioService _audioService;
+
+        [Inject]
+        private void Constructor(IAudioService audioService)
+        {
+            _audioService = audioService;
+        }
+
+        private AudioPlayer _audioPlayer;
+
+        #region MonoBehaviour
+
+        private void Awake() => _audioPlayer = new AudioPlayer(_audioService, _audioPreferences);
+
+        #endregion
+
+        private void PlaySound() => _audioPlayer.Play(transform.position);
+    }
+}
