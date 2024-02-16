90bb0ea426301a52deeab5022c23762415112bf9
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-02-16 09:40:53 +0200

    - imported android logcat;

--- Assets/Scripts/Infrastructure/Services/Advertisement/AdvertisementService.cs
+++ Assets/Scripts/Infrastructure/Services/Advertisement/AdvertisementService.cs
@@ -1,6 +1,21 @@
 ﻿using Infrastructure.Services.Advertisement.Core;
+using Infrastructure.Services.StaticData.Core;
+using Zenject;
 
 namespace Infrastructure.Services.Advertisement
 {
-    public class AdvertisementService : IAdvertisementService { }
+    public class AdvertisementService : IAdvertisementService, IInitializable
+    {
+        private readonly IStaticDataService _staticDataService;
+        
+        public AdvertisementService(IStaticDataService staticDataService)
+        {
+            _staticDataService = staticDataService;
+        }
+
+        public void Initialize()
+        {
+            
+        }
+    }
 }
