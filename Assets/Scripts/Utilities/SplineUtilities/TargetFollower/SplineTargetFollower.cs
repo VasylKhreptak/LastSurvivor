029d8d1f06a43112413bb9d450841362332f56d1
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-02-19 16:53:09 +0200

    - fixed TargetSplineFollower; - added Level 3;

--- Assets/Scripts/Utilities/SplineUtilities/TargetFollower/SplineTargetFollower.cs
+++ Assets/Scripts/Utilities/SplineUtilities/TargetFollower/SplineTargetFollower.cs
@@ -28,6 +28,13 @@ namespace Utilities.SplineUtilities.TargetFollower
         private Vector3 _targetPosition;
         private Quaternion _targetRotation;
 
+        private float _nearestTime;
+        private float _positionTime;
+        private float _rotationTime;
+        private float3 _position;
+        private float3 _tangent;
+        private float3 _up;
+
         public Transform Target { get; set; }
 
         public void Tick()
@@ -48,17 +55,24 @@ namespace Utilities.SplineUtilities.TargetFollower
         private void UpdateTargetValues()
         {
             Vector3 localPoint = _splineRoot.InverseTransformPoint(_lastTargetPosition);
-            SplineUtility.GetNearestPoint(_spline, localPoint, out float3 _, out float nearestTime);
-            float targetSplineDistance = nearestTime * _splineLength + _preferences.DistanceOffset;
-            Vector3 targetSplinePoint = _spline.GetPointAtLinearDistance(0, targetSplineDistance, out float _);
-            _targetPosition = _splineRoot.TransformPoint(targetSplinePoint);
-            _targetRotation = Quaternion.LookRotation(_spline.EvaluateTangent(nearestTime));
+            SplineUtility.GetNearestPoint(_spline, localPoint, out float3 _, out _nearestTime);
+            _positionTime = math.clamp(_nearestTime + _preferences.DistanceOffset / _splineLength, 0f, 1f);
+            _rotationTime = math.clamp(_nearestTime + _preferences.RotationDistanceOffset / _splineLength, 0f, 1f);
+            _position = _spline.EvaluatePosition(_positionTime);
+            _tangent = _spline.EvaluateTangent(_rotationTime);
+            _up = _spline.EvaluateUpVector(_rotationTime);
+            _targetPosition = _splineRoot.TransformPoint(_position);
+            _targetRotation = Quaternion.LookRotation(_tangent, _up);
             _targetRotation *= Quaternion.Euler(_preferences.RotationOffset);
         }
 
         private void Move()
         {
             _transform.position = Vector3.Lerp(_transform.position, _targetPosition, Time.deltaTime * _preferences.FollowSpeed);
+
+            if (_preferences.UpdateRotation == false)
+                return;
+
             _transform.rotation = Quaternion.Lerp(_transform.rotation, _targetRotation, Time.deltaTime * _preferences.RotateSpeed);
         }
 
@@ -81,12 +95,16 @@ namespace Utilities.SplineUtilities.TargetFollower
             [SerializeField] private float _followSpeed = 1f;
             [SerializeField] private float _rotateSpeed = 1f;
             [SerializeField] private float _distanceOffset = -5f;
+            [SerializeField] private float _rotationDistanceOffset = 0f;
             [SerializeField] private Vector3 _rotationOffset = Vector3.zero;
+            [SerializeField] private bool _updateRotation = true;
 
             public float FollowSpeed => _followSpeed;
             public float RotateSpeed => _rotateSpeed;
             public float DistanceOffset => _distanceOffset;
+            public float RotationDistanceOffset => _rotationDistanceOffset;
             public Vector3 RotationOffset => _rotationOffset;
+            public bool UpdateRotation => _updateRotation;
         }
     }
 }
