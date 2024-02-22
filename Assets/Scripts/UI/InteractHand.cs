9528d3540c3a956c9dafd10dd486d4b35ab9092d
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-02-22 11:52:11 +0200

    - added interact hand to main scene;

---
+++ Assets/Scripts/UI/InteractHand.cs
@@ -0,0 +1,50 @@
+﻿using Plugins.Animations;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Splines;
+
+namespace UI
+{
+    public class InteractHand : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private UIBehaviour _interactionTarget;
+        [SerializeField] private Transform _hand;
+        [SerializeField] private SplineContainer _splineContainer;
+
+        [Header("Preferences")]
+        [SerializeField] private float _speed = 5f;
+        [SerializeField] private FadeAnimation _fadeAnimation;
+
+        private bool _wasPressed;
+
+        private float _time;
+        private Vector3 _evaluatedPosition;
+
+        #region MonoBehaviour
+
+        private void Update()
+        {
+            _time += Time.deltaTime * _speed;
+            _time = Mathf.Repeat(_time, 1f);
+            _evaluatedPosition = _splineContainer.Spline.EvaluatePosition(_time);
+            _hand.position = _splineContainer.transform.TransformPoint(_evaluatedPosition);
+        }
+
+        private void OnEnable() => _interactionTarget.OnPointerDownAsObservable().Subscribe(_ => OnPointerDown()).AddTo(this);
+
+        #endregion
+
+        private void OnPointerDown()
+        {
+            if (_wasPressed)
+                return;
+
+            _fadeAnimation.PlayForward(() => Destroy(gameObject));
+
+            _wasPressed = true;
+        }
+    }
+}
