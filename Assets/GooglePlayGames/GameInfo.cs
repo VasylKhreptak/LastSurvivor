f23ccb134a2c179fc8fadd33c32fa5e410b45dad
DESKTOP-5QT8APO\Vasyl Khreptak <VasylKhreptak12@gmail.com>
2024-03-16 12:15:41 +0200

    - configured Google Play Services;

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -36,10 +36,10 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "1081225345728"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
-        public const string NearbyConnectionServiceId = "";
+        public const string WebClientId = "1081225345728-dtsqpc8rgahier3dn5rnckn5krjc4njp.apps.googleusercontent.com"; // Filled in automatically
+        public const string NearbyConnectionServiceId = "com.UnityTechnologies.com.fgames.lastsurvivors";
 
         public static bool ApplicationIdInitialized() {
             return !string.IsNullOrEmpty(ApplicationId) && !ApplicationId.Equals(ToEscapedToken(UnescapedApplicationId));
