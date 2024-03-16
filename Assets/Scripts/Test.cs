7e49f3bf50cd9adc4a2511f84b74c453ef5d0eab
DESKTOP-5QT8APO\Vasyl Khreptak <VasylKhreptak12@gmail.com>
2024-03-16 09:54:11 +0200

    - imported UniTask package;

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,6 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private void Awake() { }
+    private async void Awake()
+    {
+        // await DoSomething(this.GetCancellationTokenOnDestroy());
+    }
+
+    private async UniTask DoSomething(CancellationToken cancellationToken)
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(10f), cancellationToken: cancellationToken);
+        gameObject.SetActive(false);
+        await UniTask.Delay(TimeSpan.FromSeconds(10f), cancellationToken: cancellationToken);
+        gameObject.SetActive(true);
+    }
 }
